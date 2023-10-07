41b21b761b922c54f346dac6b0c3b03ddda57c20
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2023-10-07 15:48:40 +0300

    Fixed input error not appearing

--- angularapp/src/app/new-post/new-post.component.ts
+++ angularapp/src/app/new-post/new-post.component.ts
@@ -9,25 +9,20 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent {
-	titleFormControl = new FormControl("", { nonNullable: true, validators: [Validators.required] });
-	contentFormControl = new FormControl("", { nonNullable: true, validators: [Validators.required] });
+	titleFormControl = new FormControl("", {
+		nonNullable: true,
+		validators: [Validators.required, Validators.minLength(5), Validators.maxLength(150)]
+	});
+	contentFormControl = new FormControl("", {
+		nonNullable: true,
+		validators: [Validators.required, Validators.minLength(5), Validators.maxLength(40000)]
+	});
 
 	constructor(private postsService: PostsService, private router: Router) { }
 
 	createNewPost(): void {
 		this.titleFormControl.setValue(this.titleFormControl.value.trim());
-		if (this.titleFormControl.value.length == 0) {
-			this.titleFormControl.setErrors({ "required": true });
-		} else if (this.titleFormControl.value.length < 5 || this.titleFormControl.value.length > 150) {
-			this.titleFormControl.setErrors({ "invalidLength": true });
-		}
-
 		this.contentFormControl.setValue(this.contentFormControl.value.trim());
-		if (this.contentFormControl.value.length == 0) {
-			this.contentFormControl.setErrors({ "required": true });
-		} else if (this.contentFormControl.value.length < 5 || this.contentFormControl.value.length > 40000) {
-			this.contentFormControl.setErrors({ "invalidLength": true });
-		}
 
 		if (this.contentFormControl.invalid || this.titleFormControl.invalid) {
 			return;
